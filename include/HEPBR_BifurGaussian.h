0996701d43e067d5013e14adbb3b10795cf03609
Marcin Chrzaszcz <mchrzasz@cern.ch>
2019-02-06 17:59:20 +0100

    bifurcated Gaussian added

---
+++ include/HEPBR_BifurGaussian.h
@@ -0,0 +1,59 @@
+//   HEPLike: High Energy Physics Likelihoods
+//
+//   Header for construck likelihoods for Bifurcated gaussian distribution
+//
+//   author: Marcin Chrzaszcz
+//////////////////////////////////////////////////
+#ifndef HEPBR_BIFURGAUSSIAN_H
+#define HEPBR_BIFURGAUSSIAN_H
+
+//C++ headers
+#include <cmath>
+#include <limits>
+#include <fstream>
+#include <iomanip>
+
+
+//HEPLike headers
+#include "HEPStats.h"
+#include "HEPBR_Gaussian.h"
+
+//external:
+#include "yaml-cpp/yaml.h"
+#include "gsl/gsl_cdf.h"
+#include "gsl/gsl_sf_erf.h"
+#include "gsl/gsl_math.h"
+#include "gsl/gsl_sf_exp.h"
+
+class HEPBR_BifurGaussian: public HEPBR_Gaussian
+{
+
+ public:
+
+  explicit HEPBR_BifurGaussian() :  HEPBR_Gaussian() {};
+  explicit HEPBR_BifurGaussian(std::string s) :  HEPBR_Gaussian(s) { };
+  
+  
+  void read();
+  double GetChi2(double theory, double theory_err=0);
+  double GetLikelihood(double theory, double theory_err=0);
+  double GetLogLikelihood(double theory, double theory_err=0);  
+
+ private:
+  
+  double HEPCentral;
+  double HEPSigma_stat_R;
+  double HEPSigma_stat_L;
+  double HEPSigma_syst_R;
+  double HEPSigma_syst_L;
+
+  
+    
+
+  
+
+  
+};
+
+
+#endif
---
+++ src/HEPBR_BifurGaussian.cc
@@ -0,0 +1,68 @@
+//   HEPLike: High Energy Physics Likelihoods
+//
+//   Module to construck likelihoods for Bifurcated gaussian distribution
+//
+//   author: Marcin Chrzaszcz
+//////////////////////////////////////////////////
+
+
+#include <cmath>
+#include <limits>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+
+#include "HEPStats.h"
+#include "HEPConstants.h"
+#include "HEPBR_BifurGaussian.h"
+
+using namespace std;
+
+void HEPBR_BifurGaussian::read()
+{
+  if(! initialized)
+    {
+      std::cout << "TRYING TO READ WITHOUT GIVING ANY FILE!" << std::endl;
+      return;
+    }
+
+  read_standard();
+  
+  HEPCentral=config["Br"].as<double>();
+  HEPSigma_stat_L=config["Stat_L"].as<double>();
+  HEPSigma_stat_R=config["Stat_R"].as<double>();
+  if(config["Syst"] )
+    {
+      HEPSigma_syst_R=config["Syst"].as<double>();
+      HEPSigma_syst_L=config["Syst"].as<double>();   
+    }
+  else
+    {
+      HEPSigma_syst_R=config["Syst_R"].as<double>();
+      HEPSigma_syst_L=config["Syst_L"].as<double>();
+    }
+  cout<<HEPSigma_stat_R<<" "<<HEPSigma_stat_L<<" "<<HEPSigma_syst_R<<" "<<HEPSigma_syst_L<<endl;
+}
+double HEPBR_BifurGaussian::GetChi2(double theory, double theory_err)
+{
+  double delta=HEPCentral-theory;
+  double chi2=0.;
+  if(delta>0.)
+    {
+      double err2=HEPSigma_stat_L*HEPSigma_stat_L+ HEPSigma_syst_L*HEPSigma_syst_L+theory_err*theory_err;    
+      chi2=delta*delta/err2;
+    }
+  else
+    {
+      double err2=HEPSigma_stat_R*HEPSigma_stat_R+ HEPSigma_syst_R*HEPSigma_syst_R+theory_err*theory_err;
+      chi2=delta*delta/err2;
+    }
+  
+  return chi2;
+
+}
+
+
+
+
+
