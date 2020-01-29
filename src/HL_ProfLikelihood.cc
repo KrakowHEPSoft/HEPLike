9c26cbe895f38c4571aefd8fa86674d8bd97f088
Jihyun Bhom <Jihyun.Bhom@ifj.edu.pl>
2020-01-29 19:21:05 +0100

    applied the same path bug fix from HL_nDimLikelihood to HL_ProfLikelihood

--- src/HL_ProfLikelihood.cc
+++ src/HL_ProfLikelihood.cc
@@ -29,20 +29,39 @@ void HL_ProfLikelihood::Read()
 
   read_standard();
 
-  //cout<<HL_BibEntry<<endl;
+  if( config["ROOTData"])
+    {
+      HL_RootFile=config["ROOTData"].as<std::string>();
+      // the HL_RootFile is something like data/RD/... but we need the absolute path:
+      int pos=HFile.find("/data/");
+      //string path=HFile.substr (0, pos);
+      if(pos<0) std::cout<<"Error in HL_nDimLikelihood, didn't find 'data'"<<std::endl;
+      while(true)
+        {
+          int pos_new=HFile.find("/data/", pos+1);
+          if(pos_new>0)
+            {
+              pos=pos_new;
+            }
+          else break;
+        }
+      string path=HFile.substr (0, pos);
 
-  if( config["ROOTData"])  HL_RootFile=config["ROOTData"].as<std::string>();
+
+      path=path+"/"+HL_RootFile;
+      HL_RootFile=path;
+    }
   else
     {
       std::cout<<"You didn't profice a root file!!! HL_ProfLikelihood class is protesting!"<<std::endl;
     }
-  
+
   if(config["TGraphPath"]) HL_PATH=config["TGraphPath"].as<std::string>();
-  
+
   // now opening files
   f= new TFile(HL_RootFile.c_str(), "READ");
   likelihood=dynamic_cast<TGraph*>(f->Get(HL_PATH.c_str()));
-  
+
 
 
 
@@ -60,8 +79,8 @@ void HL_ProfLikelihood::Read()
 }
 double HL_ProfLikelihood::GetChi2(double theory, double theory_err=-1.)
 {
-  double log_likelihood=GetLogLikelihood(theory,theory_err); 
-  double chi2=-2.*log_likelihood; 
+  double log_likelihood=GetLogLikelihood(theory,theory_err);
+  double chi2=-2.*log_likelihood;
   return chi2;
 }
 
@@ -69,23 +88,19 @@ double HL_ProfLikelihood::GetLogLikelihood(double theory, double theory_error=-1
 {
   if(theory < xmin || theory > xmax) return -1.e10;
   double loglikelihood=(-1.)*likelihood->Eval(theory,0, "S" );
-  
+
   if(theory_error<0.){
       return loglikelihood;
   }
   // we inflate the likelihood by theory error square:
   loglikelihood*=(1.+theory_error)*(1.+theory_error);
 
-  
+
   return loglikelihood;
-  
+
 }
-double HL_ProfLikelihood::GetLikelihood(double theory, double theory_err=-1.) 
+double HL_ProfLikelihood::GetLikelihood(double theory, double theory_err=-1.)
 {
   double log_likelihood=GetLogLikelihood(theory,theory_err);
-  return gsl_sf_exp(log_likelihood);  
+  return gsl_sf_exp(log_likelihood);
 }
-
-
-
-
