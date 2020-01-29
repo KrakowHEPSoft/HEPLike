5884091c1125b72fd0349c1887fe3498a65a3234
Jihyun Bhom <Jihyun.Bhom@ifj.edu.pl>
2020-01-29 22:26:41 +0100

    lets try

--- src/HL_ProfLikelihood.cc
+++ src/HL_ProfLikelihood.cc
@@ -86,6 +86,8 @@ double HL_ProfLikelihood::GetChi2(double theory, double theory_err=-1.)
 
 double HL_ProfLikelihood::GetLogLikelihood(double theory, double theory_error=-1.)
 {
+  likelihood->Print(); 
+  
   if(theory < xmin || theory > xmax) return -1.e10;
   double loglikelihood=(-1.)*likelihood->Eval(theory,0, "S" );
   std::cout<<"Likelihood: "<<loglikelihood<<" for "<<theory<<std::endl;
