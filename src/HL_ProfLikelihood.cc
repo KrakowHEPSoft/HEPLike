0a393dbce2bbcafd78619b01ea5a121522568ebc
Tomas Gonzalo <tomas.gonzalo@kit.edu>
2022-09-05 19:43:36 +0200

    Updated ProfileLikelihood to work with text files instead of root (by Martin White)

--- src/HL_ProfLikelihood.cc
+++ src/HL_ProfLikelihood.cc
@@ -22,6 +22,10 @@ using namespace std;
 
 void HL_ProfLikelihood::Read()
 {
+
+  // Note: amended to allow text file input 
+
+  
   if(! initialized)
     {
       std::cout << "HL_ProfLikelihood Warning, TRYING TO READ WITHOUT GIVING ANY FILE!" << std::endl;
@@ -30,46 +34,91 @@ void HL_ProfLikelihood::Read()
 
   read_standard();
 
-  if( config["ROOTData"])
-    {
-      HL_RootFile=config["ROOTData"].as<std::string>();
-      // the HL_RootFile is something like data/RD/... but we need the absolute path:
-      int pos=HFile.find("/data/");
-      //string path=HFile.substr (0, pos);
-      if(pos<0) std::cout<<"Error in HL_nDimLikelihood, didn't find 'data'"<<std::endl;
-      while(true)
-        {
-          int pos_new=HFile.find("/data/", pos+1);
-          if(pos_new>0)
-            {
-              pos=pos_new;
-            }
-          else break;
-        }
-      string path=HFile.substr (0, pos);
+  //Always use text input if it is available (avoids potential mem issues)
+  bool TextInput=false;
+  if( config["TextData"])TextInput=true;
+  
+    if(TextInput){
+    std::cout << "HL_ProfLikelihood is using text input" << std::endl;
+    int pos=HFile.find("/data/");
+        //string path=HFile.substr (0, pos);
+        if(pos<0) std::cout<<"Error in HL_ProfLikelihood, didn't find 'data'"<<std::endl;
+        while(true)
+          {
+            int pos_new=HFile.find("/data/", pos+1);
+            if(pos_new>0)
+              {
+                pos=pos_new;
+              }
+            else break;
+          }
+        string path=HFile.substr (0, pos);
+                
+        std::string filename=path+"/"+config["TextData"].as<std::string>();
+        std::cout << "Opening file " << filename << std::endl;
+        std::ifstream in(filename.c_str());
+	int nxbins;
+        in >> nxbins >> xmin >> xmax;
 
+	double x[nxbins];
+	double y[nxbins];
+	
+        for(int point=0;point<nxbins+1;point++){
+	  double point_,x_,y_;
+	  in >> point_ >> x_>> y_;
+	  x[point]=x_;
+	  y[point]=y_;
+	}
+        in.close();
+	TGraph* newGraph  = new TGraph(nxbins,x,y);
+	likelihood=newGraph;
+	newGraph->GetXaxis()->SetLimits(xmin,xmax);
 
-      path=path+"/"+HL_RootFile;
-      HL_RootFile=path;
     }
-  else
-    {
-      std::cout<<"You didn't profice a root file!!! HL_ProfLikelihood class is protesting!"<<std::endl;
+        
+    else {
+  
+      if( config["ROOTData"])
+	{
+	  HL_RootFile=config["ROOTData"].as<std::string>();
+	  // the HL_RootFile is something like data/RD/... but we need the absolute path:
+	  int pos=HFile.find("/data/");
+	  //string path=HFile.substr (0, pos);
+	  if(pos<0) std::cout<<"Error in HL_nDimLikelihood, didn't find 'data'"<<std::endl;
+	  while(true)
+	    {
+	      int pos_new=HFile.find("/data/", pos+1);
+	      if(pos_new>0)
+		{
+		  pos=pos_new;
+		}
+	      else break;
+	    }
+	  string path=HFile.substr (0, pos);
+	  
+	  
+	  path=path+"/"+HL_RootFile;
+	  HL_RootFile=path;
+	}
+      else
+	{
+	  std::cout<<"You didn't profice a root file!!! HL_ProfLikelihood class is protesting!"<<std::endl;
+	}
+      
+      if(config["TGraphPath"]) HL_PATH=config["TGraphPath"].as<std::string>();
+      
+      // now opening files
+      f= new TFile(HL_RootFile.c_str(), "READ");
+      TGraph *tmp=dynamic_cast<TGraph*>(f->Get(HL_PATH.c_str())); 
+      likelihood=dynamic_cast<TGraph*>(tmp->Clone());
+      
+      tmp->Delete();
+      f->Close();
+      delete f;
+      
+      xmin=likelihood->GetXaxis()->GetXmin () ;
+      xmax=likelihood->GetXaxis()->GetXmax () ;
     }
-
-  if(config["TGraphPath"]) HL_PATH=config["TGraphPath"].as<std::string>();
-
-  // now opening files
-  f= new TFile(HL_RootFile.c_str(), "READ");
-  TGraph *tmp=dynamic_cast<TGraph*>(f->Get(HL_PATH.c_str())); 
-  likelihood=dynamic_cast<TGraph*>(tmp->Clone());
-
-  tmp->Delete();
-  f->Close();
-  delete f;
-
-  xmin=likelihood->GetXaxis()->GetXmin () ;
-  xmax=likelihood->GetXaxis()->GetXmax () ;
 
   if(config["Observables"] )
     {
@@ -86,7 +135,6 @@ void HL_ProfLikelihood::Read()
   
   fun=MyFunction();
   fun.SetLikelihood(likelihood);
-
   
 }
 double HL_ProfLikelihood::GetLogLikelihood(double theory)
